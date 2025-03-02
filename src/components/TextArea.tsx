89370af5f4d9cf46e941774c4009e233561086e1
SlaytherZR7 <slayther.zamora@epn.edu.ec>
2025-03-01 20:55:35 -0500

    add TextArea component for text input with dynamic placeholder and styles

---
+++ src/components/TextArea.tsx
@@ -0,0 +1,38 @@
+import { Form } from 'react-bootstrap'
+import { SectionType } from '../types'
+
+interface Props {
+  type: SectionType
+  loading?: boolean
+  value: string
+  onChange: (value: string) => void
+}
+
+const commonStyles = { border: 0, height: '200px' }
+
+const getPlaceholder = ({ type, loading }: { type: SectionType, loading?: boolean }) => {
+  if (type === SectionType.From) return 'Escribe el texto a traducir'
+  if (loading === true) return 'Cargando...'
+  return 'Traducción'
+}
+
+export const TextArea = ({ type, loading, value, onChange }: Props) => {
+  const styles = type === SectionType.From
+    ? commonStyles
+    : { ...commonStyles, backgroundColor: '#f8f9fa' }
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    onChange(event.target.value)
+  }
+
+  return (
+    <Form.Control
+      autoFocus={type === SectionType.From}
+      as='textarea'
+      disabled={type === SectionType.To}
+      placeholder={getPlaceholder({ type, loading })}
+      style={styles}
+      value={value}
+      onChange={handleChange}
+    />
+  )
+}
