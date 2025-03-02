19f4a4bce63ce06b019273f9397d76147d9bc3ad
SlaytherZR7 <slayther.zamora@epn.edu.ec>
2025-03-01 19:42:02 -0500

    add constants and types for language support and state management

---
+++ src/types.d.ts
@@ -0,0 +1,13 @@
+import { type AUTO_LANGUAGE, type SUPPORTED_LANGUAGES } from './constants'
+
+export type Language = keyof typeof SUPPORTED_LANGUAGES
+export type AutoLanguage = typeof AUTO_LANGUAGE
+export type FromLanguage = Language | AutoLanguage
+
+export interface State {
+  fromLanguage: FromLanguage
+  toLanguage: Language
+  fromText: string
+  result: string
+  loading: boolean
+}
