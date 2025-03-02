74a2f3491bde488c17047969709fa010785cfe84
SlaytherZR7 <slayther.zamora@epn.edu.ec>
2025-03-01 21:21:15 -0500

    implement main application layout with language selection, text input, and result display

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,94 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
+import { useStore } from './hooks/useStore'
+import { Button, Col, Container, Row, Stack } from 'react-bootstrap'
+import { LanguageSelector } from './components/LanguageSelector'
+import { SectionType } from './types.d'
+import { TextArea } from './components/TextArea'
+import { ArrowsIcon, ClipboardIcon, SpeakerIcon } from './components/Icons'
+import { AUTO_LANGUAGE, VOICE_FOR_LANGUAGE } from './constants'
 
 const App = () => {
+  const {
+    fromLanguage,
+    toLanguage,
+    fromText,
+    result,
+    loading,
+    setFromLanguage,
+    setToLanguage,
+    setFromText,
+    setResult,
+    interchangeLanguages
+  } = useStore()
+
+  const handleClipboard = () => {
+    navigator.clipboard.writeText(result).catch(() => { })
+  }
+
+  const handleSpeak = () => {
+    const utterance = new SpeechSynthesisUtterance(result)
+    utterance.lang = VOICE_FOR_LANGUAGE[toLanguage]
+    utterance.rate = 0.9
+    speechSynthesis.speak(utterance)
+  }
+
   return (
-    <div>
+    <Container fluid>
       <h1>Google Translate Clone</h1>
-    </div>
+      <Row>
+        <Col>
+          <Stack gap={2}>
+            <LanguageSelector
+              type={SectionType.From}
+              value={fromLanguage}
+              onChange={setFromLanguage}
+            />
+            <TextArea type={SectionType.From} value={fromText} onChange={setFromText} />
+          </Stack>
+        </Col>
+        <Col xs='auto'>
+          <Button
+            variant='link'
+            disabled={fromLanguage === AUTO_LANGUAGE}
+            onClick={interchangeLanguages}
+          >
+            <ArrowsIcon />
+          </Button>
+        </Col>
+        <Col>
+          <Stack gap={2}>
+            <LanguageSelector
+              type={SectionType.To}
+              value={toLanguage}
+              onChange={setToLanguage}
+            />
+            <div style={{ position: 'relative' }}>
+              <TextArea
+                type={SectionType.To}
+                value={result}
+                onChange={setResult}
+                loading={loading}
+              />
+              <div style={{ position: 'absolute', left: 0, bottom: 0, display: 'flex' }}>
+                <Button
+                  variant='link'
+                  onClick={handleClipboard}
+                >
+                  <ClipboardIcon />
+                </Button>
+                <Button
+                  variant='link'
+                  onClick={handleSpeak}
+                >
+                  <SpeakerIcon />
+                </Button>
+              </div>
+            </div>
+          </Stack>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
