c45f046e136922fd01ef2c2c9e441b622b659702
SlaytherZR7 <slayther.zamora@epn.edu.ec>
2025-03-01 19:44:17 -0500

    add action types and section types for state management

--- src/types.d.ts
+++ src/types.d.ts
@@ -11,3 +11,15 @@ export interface State {
   result: string
   loading: boolean
 }
+
+export type Action =
+  | { type: 'SET_FROM_LANGUAGE', payload: FromLanguage }
+  | { type: 'INTERCHANGE_LANGUAGES' }
+  | { type: 'SET_TO_LANGUAGE', payload: Language }
+  | { type: 'SET_FROM_TEXT', payload: string }
+  | { type: 'SET_RESULT', payload: string }
+
+export enum SectionType {
+  From = 'from',
+  To = 'to'
+}
